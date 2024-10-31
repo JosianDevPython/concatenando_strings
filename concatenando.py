f559a85d4b0836907254b23a2bf0ad733b57d3a3
JosianDevPython <josianhenrique.devpython@gmail.com>
2024-10-31 11:55:05 -0300

    Concatenando Strings

---
+++ concatenando.py
@@ -0,0 +1,5 @@
+fist_name = "ada"
+laste_name = "love lace"
+full_name = fist_name + " " + laste_name # python usa siimbolo de + para combinar strings que se chama concatenaçao
+mensagem = "Hello, " + full_name.title() + "!"
+print(mensagem)
